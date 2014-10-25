22e6177830de395e41f89024553940e0d212afa9
chiranjeevi <chiru6753@gmail.com>
2014-10-25 19:15:58 +0530

    Add Maps

---
+++ team/chiranjeevi/opt1/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Option 1</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <script type="text/javascript" src="http://code.jquery.com/jquery.js"></script>
+  <script type="text/javascript" src="js/script.js"></script>
+</head>
+<body>
+<header>
+  <h1>This is a <strong>header</strong></h1>
+</header>
+<nav>
+  <div class="container">
+    <ul>
+     <li><a href="#home">Home</i>
+      <li><a href="#about">About Us</a></li>
+      <li><a href="#services">Services</a></li>
+      <li><a href="#contact">Contact Us</a></li>
+     </ul>
+  </div>
+ </nav>
+<section id="home">
+  <article class="container">
+    <h2>Home</h2>
+   We got no food we got no money and our pets heads are falling off! Haaaaaaarry. It's because i'm green isn't it! Your entrance was good, his was better. Hey, maybe I will give you a call sometime. Your number still 911? I just heard about evans new position,good luck to you evan backstabber, bastard, i mean baxter. Hey, maybe I will give you a call sometime. Your number still 911?  
+   </article>
+  </section>
+
+<section id="about">
+  <article class="container">
+    <h2>About</h2>
+   We got no food we got no money and our pets heads are falling off! Haaaaaaarry. It's because i'm green isn't it! Your entrance was good, his was better. Hey, maybe I will give you a call sometime. Your number still 911? I just heard about evans new position,good luck to you evan backstabber, bastard, i mean baxter. Hey, maybe I will give you a call sometime. Your number still 911?  
+   </article>
+  </section>
+
+<section id="services">
+  <article class="container">
+    <h2>Services</h2>
+   We got no food we got no money and our pets heads are falling off! Haaaaaaarry. It's because i'm green isn't it! Your entrance was good, his was better. Hey, maybe I will give you a call sometime. Your number still 911? I just heard about evans new position,good luck to you evan backstabber, bastard, i mean baxter. Hey, maybe I will give you a call sometime. Your number still 911?  
+   </article>
+  </section>
+
+<section id="contact">
+  <article class="container">
+    <h2>Contact</h2>
+   We got no food we got no money and our pets heads are falling off! Haaaaaaarry. It's because i'm green isn't it! Your entrance was good, his was better. Hey, maybe I will give you a call sometime. Your number still 911? I just heard about evans new position,good luck to you evan backstabber, bastard, i mean baxter. Hey, maybe I will give you a call sometime. Your number still 911?  
+   </article>
+  </section>
+</body>
+</html>
